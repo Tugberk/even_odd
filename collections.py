c29497ff11e32174589fabeb289c9869f7a5fa82
Tk44 <1562814+Tugberk@users.noreply.github.com>
2019-05-24 13:38:44 +0300

    Update collections.py

--- collections.py
+++ collections.py
@@ -1,28 +1,11 @@
 import itertools
 
-'''
-def I(n):
-    A = []
-    for i in range(n):
-        A.append([1 if j == i else 0 for j in range(n)])
-    return A
-'''
 
-#tests:
-
-#A = I(3)
 matrix = []
 matrix.append([0,0,0])
 matrix.append([0,1,1])
 matrix.append([1,0,1])
 matrix.append([1,1,0])
-
-
-#print matrix
-
-#matrix = map(list, map(None, *matrix))
-
-
 
 
 new_list = []
@@ -36,6 +19,3 @@ for elm in new_list:
     elm = map(list, map(None, *elm))
     print elm
 
-
-#bu yukaridaki kod calisiyor ama ben bunlara columnlari istedigim gibi yani gozuktugu gibi
-#giremiyorum o yuzden baska turlu yapmaya calisacagim
